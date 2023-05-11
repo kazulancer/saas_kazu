492a131484939df287987e454016831ecb73dc79
Kelvin Kazuyoshi Ushivata Ogasawara <kazulancer@gmail.com>
2023-05-11 00:34:17 +0000

    criando ligação entre Id

--- src/main/java/models/Autor.java
+++ src/main/java/models/Autor.java
@@ -7,5 +7,25 @@ import javax.persistece.Id;
 @Table(name = "autores")
 
 public class Autor{
+
+    private int id;
+    private String nome;
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
     
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    public int getId(){
+        return Id;
+    }
+
 }
